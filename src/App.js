8a52ea19d1330d81c6e76ede6d9e67ab1172ec5a
AjaySRawat07 <ajju246455@gmail.com>
2024-03-07 23:47:22 +0530

    ii

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LandingScreen from './Screens/LandingScreen';
+
+function App() {
+  return (
+    <>
+      <LandingScreen/>
+    </>
+  );
+}
+
+export default App;
